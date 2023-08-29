6e97fca7789dc261da6daa0cce58a9f44cc8f28a
ferrazmanu <ferraz.manuela@hotmail.com>
2023-08-29 17:38:39 -0300

    document font ajusment

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,7 +33,10 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400&display=swap"
             rel="stylesheet"
